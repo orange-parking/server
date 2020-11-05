c1d56edeb5b1991a0708e8dfd8161231a3ad22b8
qoyyima22 <qoyyimafiassalam@gmail.com>
2020-11-06 01:29:41 +0700

    dikit

---
+++ firebase/index.js
@@ -0,0 +1,17 @@
+const firebase = require("firebase");
+
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+module.exports = db;
